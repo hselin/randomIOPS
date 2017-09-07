f98de5279bda4ab735dbd1faa5084d376b1c6904
Ubuntu <ubuntu@ip-172-31-28-197.us-west-2.compute.internal>
2017-09-07 03:45:19 +0000

    update run script

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import datetime
 if __name__ == '__main__':
 	startTime = datetime.datetime.now()
 	while True:
-		result = subprocess.run(['./prog'], stdout=subprocess.PIPE)
+		result = subprocess.run(['./randio', '/dev/xvdp' , '16' , '512' , '1'], stdout=subprocess.PIPE)
 		delta = datetime.datetime.now() - startTime
 		print('%s, %s' % (result.stdout.decode('utf-8'), delta.total_seconds()))
